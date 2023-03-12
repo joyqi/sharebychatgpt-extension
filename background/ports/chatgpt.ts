d133ff6491e54b22ba9cbe976578fc22bd4bc2c7
joyqi <magike.net@gmail.com>
2023-03-13 00:22:44 +0800

    Add api request

--- background/ports/chatgpt.ts
+++ background/ports/chatgpt.ts
@@ -2,15 +2,29 @@ import type { PlasmoMessaging } from '@plasmohq/messaging';
 import { fetchArticleText } from '~lib/article';
 import { __ } from '~lib/common';
 import { tokenCache } from '~lib/tokencache';
+import { v4 as uuidv4 } from 'uuid';
 
 const MAX_LENGTH = 1000;
 const CACHE_TTL = 1000 * 10;
 const CACHE_KEY = 'chatgpt-token';
+const PROMPT = 'You are a summary writer. '
+    + 'Your write summary in Chinese for the article I give you, and only output summary. '
+    + 'The summary you are writing should be less than 140 characters, and make sure it is concise and catchy. '
+    + 'Now write a summary for the following article: ';
 
 const Endpoints = {
     Auth: 'https://chat.openai.com/api/auth/session',
     Chat: 'https://chat.openai.com/backend-api'
 };
+
+async function generateQuestion() {
+    const text = await fetchArticleText(MAX_LENGTH);
+    if (!text) {
+        throw new Error(__('errorArticle'));
+    }
+
+    return PROMPT + text;
+}
 
 async function getAccessToken() {
     const [token, setToken] = await tokenCache(CACHE_KEY, CACHE_TTL);
@@ -33,7 +47,44 @@ async function getAccessToken() {
     return data.accessToken;
 }
 
-const handler: PlasmoMessaging.PortHandler = async (_, res) => {
+// Request to ChatGPT API with access token
+async function request(path: string, method: string, body: any) {
+    const token = await getAccessToken();
+    const response = await fetch(Endpoints.Chat + path, {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(body)
+    });
+
+    if (response.status === 403) {
+        throw new Error(__('errorAuth'));
+    }
+
+    return response;
+}
+
+async function ask() {
+    const body = {
+        action: 'next',
+        message: [
+            {
+                id: uuidv4(),
+                role: 'user',
+                content: {
+                    content_type: 'text',
+                    parts: [await generateQuestion()]
+                }
+            }
+        ],
+        model: 'text-davinci-002-render',
+        parent_message_id: uuidv4()
+    }
+}
+
+const handler: PlasmoMessaging.PortHandler = async (req, res) => {
     res.send(await fetchArticleText(MAX_LENGTH));
 
     setTimeout(() => {
